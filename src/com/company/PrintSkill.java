4dad72bf85ae23aa86cf843f39a4be228962c808
Azam Alamov <91742619+gthanksg@users.noreply.github.com>
2021-11-03 21:28:01 +0300

    Add files via upload

--- src/com/company/PrintSkill.java
+++ src/com/company/PrintSkill.java
@@ -5,30 +5,30 @@ import java.util.concurrent.TimeUnit;
 
 public class PrintSkill {
 
-    public static final String[] WordsEasy = {"Tomato", "Friendship", "Intelligent", "Communist", "Nasty", "Ready", "Culmination", "Potent", "Anthology", "Hack", "Narcology", "Ignorance", "Sour", "Confused", "Rocket-carrying", "Electric guitar", "Conform", "Get burned"};
-    public static final String[] WordsMedium = {"Etymology", "Subvention", "Leucopathy", "Undergroan", "Haematosis", "Interpoint", "Distringas", "Reexchange", "Volitional", "Serjeantcy", "Unpitousty", "Torpidness", "Culvertail", "Sapiential", "Funambulus", "Eliquation", "Ruffianous", "Eikosylene", "Adjectived", "Inbreaking"};
-    public static final String[] WordsHard = {"Galactometries", "Factionalising", "Eccentricities", "Dactylographer", "Cabinetworkers", "Abominableness", "Labiogressions", "Macroaggregate", "Nanotechnology", "Paddleboatings", "Quadriennially", "Easterlinesses", "Backwardations", "Xanthosiderite", "Zombifications", "Yieldabilities", "Wafflestompers", "Valedictorians", "Tachygraphical", "Karyoplasmatic"};
+    protected static final String[] WordsEasy = {"Tomato", "Friendship", "Intelligent", "Communist", "Nasty", "Ready", "Culmination", "Potent", "Anthology", "Hack", "Narcology", "Ignorance", "Sour", "Confused", "Rocket-carrying", "Electric guitar", "Conform", "Get burned"};
+    protected static final String[] WordsMedium = {"Etymology", "Subvention", "Leucopathy", "Undergroan", "Haematosis", "Interpoint", "Distringas", "Reexchange", "Volitional", "Serjeantcy", "Unpitousty", "Torpidness", "Culvertail", "Sapiential", "Funambulus", "Eliquation", "Ruffianous", "Eikosylene", "Adjectived", "Inbreaking"};
+    protected static final String[] WordsHard = {"Galactometries", "Factionalising", "Eccentricities", "Dactylographer", "Cabinetworkers", "Abominableness", "Labiogressions", "Macroaggregate", "Nanotechnology", "Paddleboatings", "Quadriennially", "Easterlinesses", "Backwardations", "Xanthosiderite", "Zombifications", "Yieldabilities", "Wafflestompers", "Valedictorians", "Tachygraphical", "Karyoplasmatic"};
 
     public static void PresentProgram() {
-        System.out.println("\nWelcome to PrintSkill! This program will help you improve your typing skills!");
-        System.out.println("First, choose the difficulty.");
+        System.err.println("\nWelcome to PrintSkill! This program will help you improve your typing skills!");
+        System.err.println("First, choose the difficulty.");
     }
 
     public static byte SelectDifficulty() {
         boolean isCorrect;
         byte difficulty = 0;
         do {
-            System.out.print("Select difficulty (1 - Easy / 2 - Medium / 3 - Hard): ");
+            System.err.print("Select difficulty (1 - Easy / 2 - Medium / 3 - Hard): ");
             Scanner sc_diff = new Scanner(System.in);
             isCorrect = true;
             try {
                 difficulty = (byte) Integer.parseInt(sc_diff.nextLine());
             } catch (NumberFormatException e) {
-                System.out.println("Enter the difficulty correctly!");
+                System.err.println("Enter the difficulty correctly!");
                 isCorrect = false;
             }
             if ((difficulty < 1 || difficulty > 3) && isCorrect) {
-                System.out.println("Enter the difficulty correctly!");
+                System.err.println("Enter the difficulty correctly!");
                 isCorrect = false;
             }
         } while (!isCorrect);
@@ -37,18 +37,18 @@ public class PrintSkill {
 
     public static void EchoDifficulty(byte difficulty) {
         if (difficulty == 1)
-            System.out.println("\nEasy difficulty was selected.");
+            System.err.println("\nEasy difficulty was selected.");
         else if (difficulty == 2)
-            System.out.println("\nMedium difficulty was selected.");
+            System.err.println("\nMedium difficulty was selected.");
         else
-            System.out.println("\nHard difficulty was selected.");
+            System.err.println("\nHard difficulty was selected.");
     }
 
     public static void OutWords() throws InterruptedException {
         TimeUnit.SECONDS.sleep(2);
-        System.out.println("\nYour words are ready. They will be withdrawn now.");
+        System.err.println("\nYour words are ready. They will be withdrawn now.");
         TimeUnit.SECONDS.sleep(2);
-        System.out.println("\nGet ready! You have 5 seconds!");
+        System.err.println("\nGet ready! You have 5 seconds!");
         TimeUnit.SECONDS.sleep(5);
     }
 
@@ -66,25 +66,25 @@ public class PrintSkill {
         for (int n = 0; n < 5; n++) {
             do {
                 byte length = (byte)Array[n].length();
-                System.out.print("\n===");
+                System.err.print("\n===");
                 for(int i = 0; i < length; i++) {
-                    System.out.print("=");
+                    System.err.print("=");
                 }
-                System.out.println("===");
-                System.out.println("   " + Array[n]);
-                System.out.print("===");
+                System.err.println("===");
+                System.err.println("   " + Array[n]);
+                System.err.print("===");
                 for(int i = 0; i < length; i++) {
-                    System.out.print("=");
+                    System.err.print("=");
                 }
-                System.out.println("===");
-                System.out.print("\nEnter the word that appeared on your screen: ");
+                System.err.println("===");
+                System.err.print("\nEnter the word that appeared on your screen: ");
                 Scanner sc_text = new Scanner(System.in);
                 TextIn = sc_text.nextLine();
                 if (TextIn.equalsIgnoreCase(Array[n])) {
-                    System.out.println("\nFine! You entered word correctly!");
+                    System.err.println("\nFine! You entered word correctly!");
                     isPrecise = true;
                 } else {
-                    System.out.println("\nEnter the word correctly, without errors and extra spaces. Penalty 3 seconds!\n");
+                    System.err.println("\nEnter the word correctly, without errors and extra spaces. Penalty 3 seconds!\n");
                     TimeUnit.SECONDS.sleep(2);
                     isPrecise = false;
                 }
@@ -94,13 +94,13 @@ public class PrintSkill {
 
     public static void CalculateResults() throws InterruptedException {
         TimeUnit.SECONDS.sleep(2);
-        System.out.print("Calculating results");
+        System.err.print("Calculating results");
         TimeUnit.SECONDS.sleep(1);
-        System.out.print(".");
+        System.err.print(".");
         TimeUnit.SECONDS.sleep(1);
-        System.out.print(".");
+        System.err.print(".");
         TimeUnit.SECONDS.sleep(1);
-        System.out.println(".");
+        System.err.println(".");
     }
 
     public static void OutResults(float result, byte difficulty) {
@@ -111,58 +111,58 @@ public class PrintSkill {
         String Satisfactory = "Your rating: Satisfactory. Practice more.";
         if (difficulty == 1) {
             if (result <= 15) {
-                System.out.println(FastestHand);
+                System.err.println(FastestHand);
             }
             else if (result < 21 && result > 16) {
-                System.out.println(Excellent);
+                System.err.println(Excellent);
             }
             else if (result < 27 && result > 22) {
-                System.out.println(Good);
+                System.err.println(Good);
             }
             else if (result < 33 && result > 28) {
-                System.out.println(Okay);
+                System.err.println(Okay);
             }
             else {
-                System.out.println(Satisfactory);
+                System.err.println(Satisfactory);
             }
         } else if (difficulty == 2) {
             if (result <= 22) {
-                System.out.println(FastestHand);
+                System.err.println(FastestHand);
             }
             else if (result < 28 && result > 23) {
-                System.out.println(Excellent);
+                System.err.println(Excellent);
             }
             else if (result < 34 && result > 29) {
-                System.out.println(Good);
+                System.err.println(Good);
             }
             else if (result < 40 && result > 35) {
-                System.out.println(Okay);
+                System.err.println(Okay);
             }
             else {
-                System.out.println(Satisfactory);
+                System.err.println(Satisfactory);
             }
         } else {
             if (result <= 26) {
-                System.out.println(FastestHand);
+                System.err.println(FastestHand);
             }
             else if (result < 34 && result > 27) {
-                System.out.println(Excellent);
+                System.err.println(Excellent);
             }
             else if (result < 42 && result > 35) {
-                System.out.println(Good);
+                System.err.println(Good);
             }
             else if (result < 50 && result > 43) {
-                System.out.println(Okay);
+                System.err.println(Okay);
             }
             else {
-                System.out.println(Satisfactory);
+                System.err.println(Satisfactory);
             }
         }
     }
 
     public static void PressAnyKey() {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Press any key to close...");
+        System.err.print("Press any key to close...");
         scan.nextLine();
     }
 
@@ -177,7 +177,7 @@ public class PrintSkill {
         long endTime = System.currentTimeMillis();
         long time = (endTime - startTime);
         float result = (float)time/1000;
-        System.out.println("\nTime spent: " + result + " seconds.");
+        System.err.println("\nTime spent: " + result + " seconds.");
         CalculateResults();
         OutResults(result, difficulty);
         PressAnyKey();
