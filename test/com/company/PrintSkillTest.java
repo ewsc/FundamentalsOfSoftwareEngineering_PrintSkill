4cd75547f1f271df5736c1ac8cc1a088a6125f42
Azam Alamov <91742619+gthanksg@users.noreply.github.com>
2021-11-04 00:07:08 +0300

    Add files via upload

---
+++ test/com/company/PrintSkillTest.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrintSkillTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void presentProgram() {
+    }
+
+    @Test
+    void selectDifficulty() {
+    }
+
+    @Test
+    void echoDifficulty() {
+    }
+
+    @Test
+    void outWords() {
+    }
+
+    @Test
+    void inputWords() {
+    }
+
+    @Test
+    void calculateResults() {
+    }
+
+    @Test
+    void outResults() {
+    }
+
+    @Test
+    void pressAnyKey() {
+    }
+
+    @Test
+    void main() {
+    }
+}
