2a670bb87add57c57aeaeec82e80f81e976dc918
PavelRomanko <89462570+PavelRomanko@users.noreply.github.com>
2021-11-09 19:15:23 +0300

    Update PrintSkill.java
    Добавлен "мультиплеер" и система сохранений

--- src/com/company/PrintSkill.java
+++ src/com/company/PrintSkill.java
@@ -1,7 +1,10 @@
 package com.company;
 
+import java.io.*;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 
 public class PrintSkill {
 
@@ -9,26 +12,70 @@ public class PrintSkill {
     protected static final String[] WordsMedium = {"Etymology", "Subvention", "Leucopathy", "Undergroan", "Haematosis", "Interpoint", "Distringas", "Reexchange", "Volitional", "Serjeantcy", "Unpitousty", "Torpidness", "Culvertail", "Sapiential", "Funambulus", "Eliquation", "Ruffianous", "Eikosylene", "Adjectived", "Inbreaking"};
     protected static final String[] WordsHard = {"Galactometries", "Factionalising", "Eccentricities", "Dactylographer", "Cabinetworkers", "Abominableness", "Labiogressions", "Macroaggregate", "Nanotechnology", "Paddleboatings", "Quadriennially", "Easterlinesses", "Backwardations", "Xanthosiderite", "Zombifications", "Yieldabilities", "Wafflestompers", "Valedictorians", "Tachygraphical", "Karyoplasmatic"};
 
+    public static String userPath;
+    public static String userName;
+
     public static void presentProgram() {
-        System.err.println("\nWelcome to PrintSkill! This program will help you improve your typing skills!");
-        System.err.println("First, choose the difficulty.");
+        System.err.println("Welcome to PrintSkill! This program will help you improve your typing skills!");
+    }
+
+    public static int getResultsNumber() throws FileNotFoundException {
+        Scanner scan = new Scanner(new File(userPath));
+        int i = 0;
+        boolean isCorrect;
+        do {
+            isCorrect = true;
+            try {
+                scan.nextLine();
+                i = i + 1;
+            } catch (NoSuchElementException e) {
+                isCorrect = false;
+            }
+        } while (isCorrect);
+        return i;
+    }
+
+    public static void updateUserInfo() {
+        System.err.print("Enter username: ");
+        Scanner in = new Scanner(System.in);
+        userName = in.nextLine();
+        userPath = "users\\" + userName + ".txt";
+        try {
+            Scanner scan = new Scanner(new File(userPath));
+            int size = getResultsNumber();
+            String [] results = new String[size];
+            int i, n;
+            for(n = 0; n < size; n++) {
+                results[n] = scan.nextLine();
+            }
+            System.err.println("Hello, " + userName + ", here are your previous result(s): \n");
+            System.err.println("===========================================================");
+            for (i = 0; i < 5; i++) {
+                size = size - 1;
+                if(size > -1)
+                System.err.println(results[size] + "\n===========================================================");
+            }
+        } catch (FileNotFoundException e){
+            System.err.println("Welcome to the club, " + userName + "!");
+            System.err.println("TIP: Use this username next time to upload the results.");
+        }
     }
 
     public static byte selectDifficulty() {
         boolean isCorrect;
         byte difficulty = 0;
+        System.err.print("Select difficulty (1 - Easy / 2 - Medium / 3 - Hard): ");
         do {
-            System.err.print("\nSelect difficulty (1 - Easy / 2 - Medium / 3 - Hard): ");
             Scanner sc_diff = new Scanner(System.in);
             isCorrect = true;
             try {
                 difficulty = (byte) Integer.parseInt(sc_diff.nextLine());
             } catch (NumberFormatException e) {
-                System.err.println("Enter the difficulty correctly!");
+                System.err.print("Enter the difficulty correctly: ");
                 isCorrect = false;
             }
             if ((difficulty < 1 || difficulty > 3) && isCorrect) {
-                System.err.println("Enter the difficulty correctly!");
+                System.err.print("Enter the difficulty correctly: ");
                 isCorrect = false;
             }
         } while (!isCorrect);
@@ -84,8 +131,8 @@ public class PrintSkill {
                     System.err.println("\nFine! You entered word correctly!");
                     isPrecise = true;
                 } else {
-                    System.err.println("\nEnter the word correctly, without errors and extra spaces. Penalty 3 seconds!\n");
-                    TimeUnit.SECONDS.sleep(2);
+                    System.err.println("\nEnter the word correctly, without errors and extra spaces. Penalty 3 seconds!");
+                    TimeUnit.SECONDS.sleep(3);
                     isPrecise = false;
                 }
             } while (!isPrecise);
@@ -94,7 +141,7 @@ public class PrintSkill {
 
     public static void calculateResults() throws InterruptedException {
         TimeUnit.SECONDS.sleep(2);
-        System.err.print("Calculating results");
+        System.err.print("Calculating results\n");
         TimeUnit.SECONDS.sleep(1);
         System.err.print(".");
         TimeUnit.SECONDS.sleep(1);
@@ -104,7 +151,7 @@ public class PrintSkill {
     }
 
     public static void outResults(float result, byte difficulty) {
-        String FastestHand = "Your rating: 'The fastest hand in the Wild West.'";
+        String FastestHand = "Your rating: " + (char)34 + "The fastest hand in the Wild West" + (char)34 + ".";
         String Good = "Your rating: Good, but it could be better!";
         String Excellent = "Your rating: Excellent! Keep it up!";
         String Okay = "Your rating: Okay, but try your best next time!";
@@ -137,14 +184,44 @@ public class PrintSkill {
     }
 
     public static void pressAnyKey() {
+        System.err.println("\nSee you space cowboy.");
         Scanner scan = new Scanner(System.in);
         System.err.print("Press any key to close...");
         scan.nextLine();
     }
 
-    public static void main (String[] args) throws InterruptedException {
+    public static boolean isTrue;
+    public static boolean isApproved() {
+        boolean isCorrect;
+        String yn;
+        System.err.print("(Y - Yes / N - No): ");
+        do {
+            isCorrect = true;
+            Scanner in = new Scanner (System.in);
+            yn = in.nextLine();
+            if (yn.equalsIgnoreCase("Y"))
+                isTrue = true;
+            else if (yn.equalsIgnoreCase("N"))
+                isTrue = false;
+            else{
+                isCorrect = false;
+                System.err.print("Choose correctly (Y / N): ");
+            }
+        }while(!isCorrect);
+        return isTrue;
+    }
+
+    public static String wordDifficulty(byte difficulty) {
+        if (difficulty == 1)
+            return "Easy";
+        else if (difficulty == 2)
+            return "Medium";
+        else
+            return "Hard";
+    }
+
+    public static void singleSession() throws InterruptedException, IOException {
         byte difficulty;
-        presentProgram();
         difficulty = selectDifficulty();
         echoDifficulty(difficulty);
         outWords();
@@ -154,8 +231,63 @@ public class PrintSkill {
         long time = (endTime - startTime);
         float result = (float)time/1000;
         System.err.println("\nTime spent: " + result + " seconds.");
+        FileWriter writer = new FileWriter(userPath, true);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        writer.write(dtf.format(now) + " || Difficulty(" + wordDifficulty(difficulty) + "): " + result + " seconds.\n");
+        writer.close();
         calculateResults();
         outResults(result, difficulty);
+    }
+
+    public static void multiSession() throws InterruptedException {
+        byte difficulty;
+        difficulty = selectDifficulty();
+        echoDifficulty(difficulty);
+        System.err.print("\nPlayer 1.");
+        outWords();
+        long startTime = System.currentTimeMillis();
+        inputWords(difficulty);
+        long endTime = System.currentTimeMillis();
+        long time = (endTime - startTime);
+        float result1 = (float)time/1000;
+        System.err.println("\nIt's time to swap, players!");
+        TimeUnit.SECONDS.sleep(10);
+        System.err.print("\nPlayer 2.");
+        outWords();
+        startTime = System.currentTimeMillis();
+        inputWords(difficulty);
+        endTime = System.currentTimeMillis();
+        time = (endTime - startTime);
+        float result2 = (float)time/1000;
+        System.err.println("Player 1: " + result1 + "\nPlayer 2: " + result2);
+        calculateResults();
+        //Zaurs results
+    }
+
+    public static void session() throws IOException, InterruptedException {
+        System.err.print("\nDo you want to initiate multiplayer? ");
+        if (isApproved()){
+            System.err.println("Multi session is active now. (Saves disabled)\n");
+            do {
+                multiSession();
+                System.err.print("\nDo you want to continue this session? ");
+                isApproved();
+            } while (isTrue);
+        } else {
+            System.err.println("Single session is active now.\n");
+            do {
+                singleSession();
+                System.err.print("\nDo you want to continue this session? ");
+                isApproved();
+            } while (isTrue);
+        }
+    }
+
+    public static void main (String[] args) throws InterruptedException, IOException {
+        presentProgram();
+        updateUserInfo();
+        session();
         pressAnyKey();
     }
 }
